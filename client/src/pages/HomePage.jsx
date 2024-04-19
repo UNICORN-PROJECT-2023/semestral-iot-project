3f7f5f76001bea45c8830cade11c5b8cffb8a3b6
Michal Šváb <michal.svab@blindspot.ai>
2024-04-19 22:25:23 +0200

    add simple graph view

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useRef } from 'react';
+
 
 const StyledWrapper = styled.div`
     display: flex;
--- client/src/screens/WarehouseDetail.jsx
+++ client/src/screens/WarehouseDetail.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Typography, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import {Container, Typography, Table, TableBody, TableCell, TableHead, TableRow} from '@mui/material';
 import ApiService from "../services/apiService";
 import styled from "styled-components";
 import EditWarehouseModal from "./EditWarehouseModal";
+import GraphScreen from "./GraphScreen";
+import { motion } from "framer-motion";
 
 const StyledWrapper = styled.div`
     display: flex;
@@ -14,6 +16,18 @@ const StyledWrapper = styled.div`
     padding-top: 140px;
     background-color: #0D1117;
 
+    button {
+        color: #fff;
+        border: none;
+        padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+        font-size: 1rem;
+        letter-spacing: 1px;
+        font-weight: bold;
+        cursor: ${props => props.disabled ? '' : 'pointer'};
+        background-image: linear-gradient(${props => props.disabled ? '#d3d3d3' : '#5c5c5c'}, ${props => props.disabled ? '#a8a8a8' : 'rgba(0,0,0,0.18)'});
+    }
+    
     @media (max-width: 768px) {
         padding: 60px 2rem 0;
     }
@@ -24,6 +38,8 @@ const WarehouseDetail = () => {
     const { warehouseId } = useParams();
     const [records, setRecords] = useState([]);
     const [warehouse, setWarehouse] = useState(null);
+    const [viewMode, setViewMode] = useState('table');  // Default view mode
+
 
     useEffect(() => {
         fetchWarehouseDetails();
@@ -51,6 +67,10 @@ const WarehouseDetail = () => {
         }
     };
 
+    const toggleViewMode = () => {
+        setViewMode(prevMode => prevMode === 'table' ? 'graph' : 'table');
+    };
+
     // Method to refresh the warehouse details after update
     const handleUpdate = () => {
         fetchWarehouseDetails();
@@ -61,47 +81,59 @@ const WarehouseDetail = () => {
             <Container>
                 {warehouse && (
                     <>
-                        <Typography variant="h4" sx={{ mb: 2 }}>
+
+                        <Typography variant="h4" sx={{mb: 2}}>
                             Warehouse-{warehouse.id}
                         </Typography>
-                        <Typography variant="body1" sx={{ mb: 1 }}>
+                        <Typography variant="body1" sx={{mb: 1}}>
                             Min Temperature: {warehouse.temperatureMin}°C
                         </Typography>
-                        <Typography variant="body1" sx={{ mb: 1 }}>
+                        <Typography variant="body1" sx={{mb: 1}}>
                             Max Temperature: {warehouse.temperatureMax}°C
                         </Typography>
-                        <Typography variant="body1" sx={{ mb: 1 }}>
+                        <Typography variant="body1" sx={{mb: 1}}>
                             Alert Min Duration: {warehouse.allertMinDuration} minutes
                         </Typography>
-                        <EditWarehouseModal
-                            apiService={apiService}
-                            warehouseId={warehouseId}
-                            initialData={{
-                                minTemperature: warehouse.temperatureMin,
-                                maxTemperature: warehouse.temperatureMax,
-                                alertDuration: warehouse.allertMinDuration
-                            }}
-                            onUpdated={handleUpdate}
-                        />
+                        <div style={{justifyContent: 'flex-end', display: 'flex', marginTop: '20px'}}>
+                            <EditWarehouseModal
+                                apiService={apiService}
+                                warehouseId={warehouseId}
+                                initialData={{
+                                    minTemperature: warehouse.temperatureMin,
+                                    maxTemperature: warehouse.temperatureMax,
+                                    alertDuration: warehouse.allertMinDuration
+                                }}
+                                onUpdated={handleUpdate}
+                            />
+                            <motion.button whileHover={{scale: 1.1}}
+                                           whileTap={{scale: 0.9}}
+                                           onClick={toggleViewMode}
+                            >
+                                {viewMode === 'table' ? 'Show Graph' : 'Show Table'}
+                            </motion.button>
+                        </div>
                     </>
                 )}
-
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell sx={{ color: 'white', fontWeight: 'bold', fontSize: '1.1rem' }}>Date</TableCell>
-                            <TableCell sx={{ color: 'white', fontWeight: 'bold', fontSize: '1.1rem' }}>Temperature (°C)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {records.map((record) => (
-                            <TableRow key={record.id}>
-                                <TableCell sx={{ color: 'white' }}>{new Date(record.date).toLocaleString()}</TableCell>
-                                <TableCell sx={{ color: 'white' }}>{record.temperature}</TableCell>
+                {viewMode === 'table' ? (
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ color: 'white', fontWeight: 'bold', fontSize: '1.1rem' }}>Date</TableCell>
+                                <TableCell sx={{ color: 'white', fontWeight: 'bold', fontSize: '1.1rem' }}>Temperature (°C)</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                        </TableHead>
+                        <TableBody>
+                            {records.map((record) => (
+                                <TableRow key={record.id}>
+                                    <TableCell sx={{ color: 'white' }}>{new Date(record.date).toLocaleString()}</TableCell>
+                                    <TableCell sx={{ color: 'white' }}>{record.temperature}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                ) : (
+                    <GraphScreen records={records.toReversed()} minTemperature={warehouse.temperatureMin} maxTemperature={warehouse.temperatureMax} />
+                )}
             </Container>
         </StyledWrapper>
     );
